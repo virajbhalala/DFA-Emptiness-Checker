397fc6e7fbcbeedcbc54f0654d8de753bc3c8933
Viraj Bhalala <virajbhalala@gmail.com>
2015-04-20 16:17:31 -0400

    few basic declaration

--- src/p3_15s_vb97.java
+++ src/p3_15s_vb97.java
@@ -1,6 +1,30 @@
 import java.util.*;
-
 public class p3_15s_vb97 {
 	
+	//5 items
+	public static String Q="";
+	//here alphabet=sigma notation
+	public static String alphabet="";
+	//here transition =delta notation
+	public static String transition="";
+	//start state
+	public static String Q_o="";
+	//final state
+	public static String F="";
+	
+	
+	public static LinkedList S = new LinkedList(); 
+	
+	public static void main(String[] args){
+		Scanner s =new Scanner(System.in);
+		//assumed user input DFA in the given format.
+		String input = s.nextLine();
+		
+		//copies the input
+		String temp =input;
+		
+		
+	}
+
 
 }
