96dfe578ec2d880e4a6c804dc476e72b70320630
Viraj Bhalala <virajbhalala@gmail.com>
2015-04-20 17:13:27 -0400

    transition fnction

--- src/p3_15s_vb97.java
+++ src/p3_15s_vb97.java
@@ -13,7 +13,9 @@ public class p3_15s_vb97 {
 	public static String F="";
 	
 	
-	public static LinkedList S = new LinkedList(); 
+	public static List<String> S = new ArrayList<String>();
+	public static List<String> statesList =new ArrayList<String>();
+	public static List<String> alphabetsList =new ArrayList<String>();
 	
 	public static void main(String[] args){
 		Scanner s =new Scanner(System.in);
@@ -25,9 +27,8 @@ public class p3_15s_vb97 {
 		String temp =input;
 		//{q0,q1,...,qn},{l1,l2,...,lr},{(q0,l1,qi0,1),(q0,l2,qi0,2),...,(qn,lr,qin,r)},q0,{qj1,qj2,...,qjs}
 		
-		
 		//parsing begins here
-		Q=temp.substring(1, temp.indexOf('}'));
+		Q =temp.substring(1, temp.indexOf('}'));
 		System.out.println("List of States in Q: "+Q +"\n");
 		
 		// added +2 so that it skips begining ",{"
@@ -35,12 +36,20 @@ public class p3_15s_vb97 {
 		
 		alphabet=temp.substring(1, temp.indexOf('}'));
 		System.out.println("List of Symbols in Aplhabet: "+ alphabet + "\n");
+		String [] a=alphabet.split(",");
+		alphabetsList= Arrays.asList(a);
+		//System.out.println ("test: " + alphabetsList);
 		
 		temp=temp.substring(temp.indexOf('}')+2);
 		
+		//transition part starts here
 		transition=temp.substring(1, temp.indexOf('}'));
 		System.out.println(transition);
+
+		System.out.println("state \t" + alphabetsList.get(0) + "\t" + alphabetsList.get(1) +"\n");
 		
+		
+		//transition part ends here
 		temp=temp.substring(temp.indexOf('}')+2);
 		
 		Q_o=temp.substring(0, 2);
@@ -51,6 +60,8 @@ public class p3_15s_vb97 {
 		F=temp.substring(1, temp.indexOf('}'));
 		System.out.println("List of accepting states: "+F + "\n");
 		//parsing ends
+		
+		
 	}
 
 
