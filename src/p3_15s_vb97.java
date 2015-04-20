c38301f0e95e7f2d4d67771f170eecab0a761602
Viraj Bhalala <virajbhalala@gmail.com>
2015-04-20 16:40:19 -0400

    parsing finished

--- src/p3_15s_vb97.java
+++ src/p3_15s_vb97.java
@@ -19,11 +19,38 @@ public class p3_15s_vb97 {
 		Scanner s =new Scanner(System.in);
 		//assumed user input DFA in the given format.
 		String input = s.nextLine();
+		System.out.println(input + "\n");
 		
 		//copies the input
 		String temp =input;
+		//{q0,q1,...,qn},{l1,l2,...,lr},{(q0,l1,qi0,1),(q0,l2,qi0,2),...,(qn,lr,qin,r)},q0,{qj1,qj2,...,qjs}
 		
 		
+		//parsing begins here
+		Q=temp.substring(1, temp.indexOf('}'));
+		System.out.println("List of States in Q: "+Q +"\n");
+		
+		// added +2 so that it skips begining ",{"
+		temp=temp.substring(temp.indexOf('}')+2);
+		
+		alphabet=temp.substring(1, temp.indexOf('}'));
+		System.out.println("List of Symbols in Aplhabet: "+ alphabet + "\n");
+		
+		temp=temp.substring(temp.indexOf('}')+2);
+		
+		transition=temp.substring(1, temp.indexOf('}'));
+		System.out.println(transition);
+		
+		temp=temp.substring(temp.indexOf('}')+2);
+		
+		Q_o=temp.substring(0, 2);
+		System.out.println("Starting state: "+Q_o + "\n");
+		
+		temp=temp.substring(3);
+		
+		F=temp.substring(1, temp.indexOf('}'));
+		System.out.println("List of accepting states: "+F + "\n");
+		//parsing ends
 	}
 
 
