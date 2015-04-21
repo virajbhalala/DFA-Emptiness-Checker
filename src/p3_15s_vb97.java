63a4680262a089973732e5912bff8576a9b0615b
Viraj Bhalala <virajbhalala@gmail.com>
2015-04-20 23:54:54 -0400

    transition table

--- src/p3_15s_vb97.java
+++ src/p3_15s_vb97.java
@@ -2,10 +2,11 @@ import java.util.*;
 public class p3_15s_vb97 {
 	
 	//5 items
-	public static String Q="";
+	//public static String Q="";
+	public static List<String> Q = new ArrayList<String>();
 	//here alphabet=sigma notation
-	public static String alphabet="";
-	public static String transition="";
+	public static List<String> alphabet = new ArrayList<String>();
+	public static String transitionString="";
 	//start state
 	public static String Q_o="";
 	//final state
@@ -13,7 +14,7 @@ public class p3_15s_vb97 {
 	
 	
 	public static List<String> S = new ArrayList<String>();
-	public static List<String> statesList =new ArrayList<String>();
+	//public static List<String> statesList =new ArrayList<String>();
 	public static List<String> alphabetsList =new ArrayList<String>();
 	public static String [] array1;
 	public static String[][] array2 = new String [100][100];
@@ -30,36 +31,25 @@ public class p3_15s_vb97 {
 		//{q0,q1,...,qn},{l1,l2,...,lr},{(q0,l1,qi0,1),(q0,l2,qi0,2),...,(qn,lr,qin,r)},q0,{qj1,qj2,...,qjs}
 		
 		//parsing begins here
-		Q =temp.substring(1, temp.indexOf('}'));
+		Q =Arrays.asList(temp.substring(1, temp.indexOf('}')).split(","));
 		System.out.println("List of States in Q: "+Q +"\n");
-		
-		statesList= Arrays.asList(Q);
-		System.out.println ("test: " + statesList);
-		
+				
 		
 		// added +2 so that it skips begining ",{"
 		temp=temp.substring(temp.indexOf('}')+2);
 		
-		alphabet=temp.substring(1, temp.indexOf('}'));
-		System.out.println("List of Symbols in Aplhabet: "+ alphabet + "\n");
-		array1=alphabet.split(",");
-		alphabetsList= Arrays.asList(array1);
+		alphabet=Arrays.asList(temp.substring(1, temp.indexOf('}')).split(","));
+		System.out.println("List of Symbols in Language: "+ alphabet + "\n");
+		//array1=alphabet.split(",");
+		//alphabetsList= Arrays.asList(array1);
 		//System.out.println ("test: " + alphabetsList);
 		
 		temp=temp.substring(temp.indexOf('}')+2);
-		
-		//transition part starts here
-		transition=temp.substring(1, temp.indexOf('}'));
+
+		transitionString=temp.substring(1, temp.indexOf('}'));
 		
 		PrintTransition();
-		
-		//printing transition table
-		//System.out.println("state \t" + alphabetsList.get(0) + "\t" + alphabetsList.get(1));
-		
-		
-		
-		
-		//transition part ends here
+	
 		temp=temp.substring(temp.indexOf('}')+2);
 		
 		Q_o=temp.substring(0, 2);
@@ -77,10 +67,14 @@ public class p3_15s_vb97 {
 	
 	public static void PrintTransition(//String transition,List<String> statesList, List<String> alphabetsList 
 			){
-		System.out.println(transition);
-		System.out.println("state \t" + alphabetsList.get(0) + "\t" + alphabetsList.get(1));
+		
+		/*
+		 * breaking each state and letter and placing them in 2D array starts here
+		 */
+		//System.out.println(transitionString);
+		//System.out.println("state \t" + alphabet.get(0) + "\t" + alphabet.get(1));
 		List<String> eachTransition = new ArrayList<String>();
-		array1=transition.split("[)],");
+		array1=transitionString.split("[)],");
 		List<String> TempList =new ArrayList<String>();
 		Arrays.fill(array2[0], null);		
 		for (int i=0; i<array1.length; i++){
@@ -100,12 +94,33 @@ public class p3_15s_vb97 {
 				array2[i][j] = TempList.get(j);
 				//System.out.print(array2[i][j] + "\t");
 			}
-			System.out.println();
+			//System.out.println();
+		}
+		/*
+		 * Ends here
+		 */
+		
+		/*
+		 * Making transition table starts here
+		 */
+		
+		//did +1 so that first colum can be written as states and heading
+		String[][] table = new String[Q.size()+1][alphabet.size()+1];
+		//fills up the header
+		table[0][0] = "states";
+		for (int i=0; i<alphabet.size();i++){
+			table[0][i+1]= alphabet.get(i);
+		}
+		System.out.println(table[0][0] +"\t"+table[0][1] +"\t"+table[0][2]);
+		
+		//fills up the lsft side
+		for (int i=0; i<Q.size();i++){
+			table[i+1][0]= Q.get(i);
 		}
 		
 		
 		eachTransition = Arrays.asList(array1);
-		  
+		 
 		  
 		  
 		  
